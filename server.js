6816ce20173c3d4367ed61418a5d28adefec7034
manhhuy01 <manhhuy0112@gmail.com>
2020-12-04 21:01:29 +0700

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const next = require('next')
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port = 3000, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
